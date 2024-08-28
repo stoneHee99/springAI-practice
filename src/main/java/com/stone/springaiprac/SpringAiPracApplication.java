48d5bf4733916da31f93d9236025a620e3eab647
Seokhee Park <seokhee_park@nexcubecorp.com>
2024-08-28 13:00:59 +0900

    init project

---
+++ src/main/java/com/stone/springaiprac/SpringAiPracApplication.java
@@ -0,0 +1,13 @@
+package com.stone.springaiprac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAiPracApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAiPracApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/stone/springaiprac/SpringAiPracApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stone.springaiprac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAiPracApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
